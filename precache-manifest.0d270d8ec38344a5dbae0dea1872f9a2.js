cd5c788bcc9023e4fee0e2fee771f66ca14bd9c7
Somat Thakali <sthakal000@citymail.cuny.edu>
2019-05-31 21:40:21 -0400

    Updates

---
+++ precache-manifest.0d270d8ec38344a5dbae0dea1872f9a2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69afb372f879cd8ea79fc78a236eacb8",
+    "url": "/SomatThakali/reactmemorygame/index.html"
+  },
+  {
+    "revision": "15006ab2b6b377e0297b",
+    "url": "/SomatThakali/reactmemorygame/static/css/main.c29c825c.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/SomatThakali/reactmemorygame/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "15006ab2b6b377e0297b",
+    "url": "/SomatThakali/reactmemorygame/static/js/main.d12d7103.chunk.js"
+  },
+  {
+    "revision": "3a94e7fffe60ff8e8acd",
+    "url": "/SomatThakali/reactmemorygame/static/js/runtime~main.4f936837.js"
+  },
+  {
+    "revision": "4d5e65a0c53ec380d408bfde1d8ff703",
+    "url": "/SomatThakali/reactmemorygame/static/media/beth.4d5e65a0.png"
+  },
+  {
+    "revision": "612ba8d4e89b5cbe07b279808234f834",
+    "url": "/SomatThakali/reactmemorygame/static/media/birdperson.612ba8d4.png"
+  },
+  {
+    "revision": "0657e1f4c5b5eb7d25b4bd18811256a3",
+    "url": "/SomatThakali/reactmemorygame/static/media/evilmorty.0657e1f4.png"
+  },
+  {
+    "revision": "978b38471c32626814803817d75f4ab8",
+    "url": "/SomatThakali/reactmemorygame/static/media/gianthead.978b3847.png"
+  },
+  {
+    "revision": "3774b39dcbda08977c789663f6049c70",
+    "url": "/SomatThakali/reactmemorygame/static/media/goldenford.3774b39d.png"
+  },
+  {
+    "revision": "af8e8a84f14297f7c14073059a4b9246",
+    "url": "/SomatThakali/reactmemorygame/static/media/jerry.af8e8a84.png"
+  },
+  {
+    "revision": "a9644830b29212e0af6fb0a353c1c46d",
+    "url": "/SomatThakali/reactmemorygame/static/media/jessica.a9644830.png"
+  },
+  {
+    "revision": "9667b7eeed7cbde4439b6b6bba4a040f",
+    "url": "/SomatThakali/reactmemorygame/static/media/meeseeks.9667b7ee.png"
+  },
+  {
+    "revision": "244deeda3690049cebc0046457a6e1cf",
+    "url": "/SomatThakali/reactmemorygame/static/media/morty.244deeda.png"
+  },
+  {
+    "revision": "d61b2d5bd91638427b6604f6f7dcf1dc",
+    "url": "/SomatThakali/reactmemorygame/static/media/mr.d61b2d5b.png"
+  },
+  {
+    "revision": "06b6584198e9fce2075bc1a4a9bfae92",
+    "url": "/SomatThakali/reactmemorygame/static/media/rick.06b65841.png"
+  },
+  {
+    "revision": "afdbb7bfad6a6acefaafbf9fb072f0cb",
+    "url": "/SomatThakali/reactmemorygame/static/media/summer.afdbb7bf.png"
+  }
+]);
